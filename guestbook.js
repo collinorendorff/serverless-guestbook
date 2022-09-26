44a5860c8c48de6228aacd4469852a041ceaa8b0
Collin Orendorff <31454647+collinorendorff@users.noreply.github.com>
2022-09-26 15:18:21 -0400

    added API URL from Cloudant sequences

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-east.functions.appdomain.cloud/api/v1/web/Orendorff%20Inc_dev/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
